4295237765007e39779c67513ab5def966ce0c31
Dhruv-Ayare <81720288+Dhruv-Ayare@users.noreply.github.com>
2021-10-29 20:52:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+var canvas=new fabric.Canvas("myCanvas");
+block_image_width=30;
+block_image_height=30;
+player_X=10;
+player_Y=10;
+var player_object="";
+var block_image_object="";
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_Y,
+            left:player_X
+        });
+        canvas.add(player_object);
+    });
+}
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_Y,
+            left:player_X
+        });
+        canvas.add(block_image_object);
+    });
+}
+window.addEventListener("keydown",myKeyDown);
+function myKeyDown(e){
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey&&keyPressed=="80"){
+        console.log("p+shift");
+        block_image_width=block_image_width+10;
+        block_image_height=block_image_height+10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(e.shiftKey&&keyPressed=="77"){
+        console.log("m+shift");
+        block_image_width=block_image_width-10;
+        block_image_height=block_image_height-10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(keyPressed=="38"){
+        up();
+        console.log("up");
+    }
+    if(keyPressed=="40"){
+        down();
+        console.log("down");
+    }
+    if(keyPressed=="37"){
+        left();
+        console.log("left");
+    }
+    if(keyPressed=="39"){
+        right();
+        console.log("right");
+    }
+    if(keyPressed=="67"){
+        new_image("cloud.png");
+        console.log("cloud");
+    }
+    if(keyPressed=="68"){
+        new_image("dark_leaves.png");
+        console.log("dark_leaves");
+    }
+    if(keyPressed=="71"){
+        new_image("ground.png");
+        console.log("ground");
+    }
+    if(keyPressed=="76"){
+        new_image("leaf.png");
+        console.log("leaf");
+    }
+    if(keyPressed=="82"){
+        new_image("roof.jpg");
+        console.log("roof");
+    }
+    if(keyPressed=="84"){
+        new_image("oak_log.png");
+        console.log("trunk");
+    }
+    if(keyPressed=="85"){
+        new_image("unique.png");
+        console.log("unique");
+    }
+    if(keyPressed=="87"){
+        new_image("wall.jpg");
+        console.log("wall");
+    }
+    if(keyPressed=="89"){
+        new_image("sandstone.png");
+        console.log("Sandstone");
+    }
+}
+function up(){
+    if(player_Y>=0){
+        player_Y=player_Y-block_image_height;
+        console.log("block_image_height="+block_image_height);
+        console.log("player_X="+player_X+",player_Y="+player_Y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if(player_Y<=500){
+        player_Y=player_Y+block_image_height;
+        console.log("block_image_height="+block_image_height);
+        console.log("player_X="+player_X+",player_Y="+player_Y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_X>=0){
+        player_X=player_X-block_image_width;
+        console.log("block_image_width="+block_image_width);
+        console.log("player_X="+player_X+",player_Y="+player_Y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_X<=850){
+        player_X=player_X+block_image_width;
+        console.log("block_image_width="+block_image_width);
+        console.log("player_X="+player_X+",player_Y="+player_Y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
